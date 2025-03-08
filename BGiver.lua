85b6f6f5f1a7ffc201129107c4a4040d610aeaa4
eo-eo-eo <eduardosipov13@outlook.com>
2025-03-08 02:37:14 +0000

    Update BGiver.lua

--- BGiver.lua
+++ BGiver.lua
@@ -1,1 +1,15 @@
+local args = {
+    [1] = "Easy"
+}
 
+game:GetService("ReplicatedStorage").Events.GetBadge:FireServer(unpack(args))
+local args = {
+    [1] = "Normal"
+}
+
+game:GetService("ReplicatedStorage").Events.GetBadge:FireServer(unpack(args))
+local args = {
+    [1] = "Hard"
+}
+
+game:GetService("ReplicatedStorage").Events.GetBadge:FireServer(unpack(args))
